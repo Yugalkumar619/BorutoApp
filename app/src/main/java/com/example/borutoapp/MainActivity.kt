0408522c725b23c3c82f2ab84ec863bc5cf058fb
yugal <yugalkumar619@gmail.com>
2021-12-31 15:36:25 +0530

    Created the project add all required dependencies Create Screen class NavGraph file Hero Model and Constants.

---
+++ app/src/main/java/com/example/borutoapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.borutoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.borutoapp.navigation.SetupNavGraph
+import com.example.borutoapp.ui.theme.BorutoAppTheme
+
+class MainActivity : ComponentActivity() {
+    
+    private lateinit var navController: NavHostController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BorutoAppTheme {
+                
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
+        }
+    }
+}
