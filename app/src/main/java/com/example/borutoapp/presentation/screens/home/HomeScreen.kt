8ec01f0258a669740fae4486ba126b4f0ff21e2b
yugal <yugalkumar619@gmail.com>
2022-01-03 19:58:52 +0530

    GetAllHeroesUseCase.kt class created with new use cases

--- app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -2,10 +2,14 @@ package com.example.borutoapp.presentation.screens.home
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 
 @Composable
-fun HomeScreen() {
-
+fun HomeScreen(
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+    val allHeroes = homeViewModel.getAllHero.collectAsLazyPagingItems()
     Scaffold(
         topBar = {
             HomeTopBar(onSearchClicked = {})
