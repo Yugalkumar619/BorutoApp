a733103093e0aaae205c7cf4c1556753a815cda5
yugal <yugalkumar619@gmail.com>
2022-01-06 13:35:06 +0530

    Created Animation Shimmer Effect

--- app/src/main/java/com/example/borutoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/borutoapp/ui/theme/Color.kt
@@ -13,6 +13,10 @@ val LightGray = Color(0xFFD8D8D8)
 val DarkGrey = Color(0xFF2A2A2A)
 val StarColor = Color(0xFFFFC94D)
 
+val shimmerLightGray = Color(0xFFF1F1F1)
+val shimmerMediumGray = Color(0xFFE3E3E3)
+val shimmerDarkGray = Color(0xFF1D1D1D)
+
 val Colors.welcomeScreenBackgroundColor
 @Composable
 get() = if (isLight) Color.White else Color.Black
