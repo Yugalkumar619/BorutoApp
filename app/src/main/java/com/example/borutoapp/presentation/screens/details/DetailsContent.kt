2fc9352193391b925b6f6f774b0673ac7334f71e
yugal <yugalkumar619@gmail.com>
2022-01-08 10:24:44 +0530

    Implement Image in Details Screen

--- app/src/main/java/com/example/borutoapp/presentation/screens/details/DetailsContent.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/details/DetailsContent.kt
@@ -1,26 +1,34 @@
 package com.example.borutoapp.presentation.screens.details
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.example.borutoapp.R
 import com.example.borutoapp.domain.model.Hero
 import com.example.borutoapp.presentation.components.InfoBox
 import com.example.borutoapp.presentation.components.OrderedList
 import com.example.borutoapp.ui.theme.*
 import com.example.borutoapp.util.Constants.ABOUT_TEXT_MAX_LINES
+import com.example.borutoapp.util.Constants.BASE_URL
 
 
+@ExperimentalCoilApi
 @ExperimentalMaterialApi
 @Composable
 fun DetailsContent(
@@ -37,7 +45,15 @@ fun DetailsContent(
         sheetContent = {
             selectedHero?.let { it1 -> BottomSheetContent(selectedHero = it1) }
         },
-        content = {}
+        content = {
+            selectedHero?.image?.let { hero ->
+                BackgroundContent(
+                    heroImage = hero,
+                    onCloseClicked = {
+                        navController.popBackStack()
+                    })
+            }
+        }
     )
 }
 
@@ -136,6 +152,51 @@ fun BottomSheetContent(
                 items = selectedHero.natureTypes,
                 textColor = contentColor
             )
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun BackgroundContent(
+    heroImage: String,
+    imageFraction: Float = 1f,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    onCloseClicked: () -> Unit
+){
+    val imageUrl = "$BASE_URL${heroImage}"
+    val painter = rememberImagePainter(imageUrl){
+        error(R.drawable.placeholder)
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = imageFraction)
+                .align(Alignment.TopStart),
+            painter = painter, 
+            contentDescription = stringResource(id = R.string.hero_image),
+            contentScale = ContentScale.Crop
+        )
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+                ){
+            IconButton(
+                modifier = Modifier.padding(all = SMALL_PADDING),
+                onClick = {onCloseClicked()}
+            ) {
+                Icon(
+                    modifier = Modifier.size(INFO_ICON_SIZE),
+                    imageVector = Icons.Default.Close, 
+                    contentDescription = stringResource(R.string.close_icon),
+                    tint = Color.White
+                )
+            }
         }
     }
 }
