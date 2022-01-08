93561b86370baf219cbfb9c3c243cc9786382ea2
yugal <yugalkumar619@gmail.com>
2022-01-08 11:45:22 +0530

    Change the radius of bottom sheet during animation

--- app/src/main/java/com/example/borutoapp/presentation/screens/details/DetailsContent.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/details/DetailsContent.kt
@@ -1,15 +1,18 @@
 package com.example.borutoapp.presentation.screens.details
 
 import android.util.Log
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.material.BottomSheetValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
@@ -18,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
@@ -43,9 +47,20 @@ fun DetailsContent(
     )
 
     val currentSheetFraction = scaffoldState.currentSheetFraction
-    Log.d("Fraction NEW", currentSheetFraction.toString())
+
+    val radiysAnim by animateDpAsState(
+        targetValue =
+        if (currentSheetFraction == 1f)
+            EXTRA_LARGE_PADDING
+        else
+            EXPANDED_RADIUS_LEVEL
+    )
 
     BottomSheetScaffold(
+        sheetShape = RoundedCornerShape(
+            topStart = radiysAnim,
+            topEnd = radiysAnim
+        ),
         scaffoldState = scaffoldState,
         sheetPeekHeight = MINIMUM_SHEET_HEIGHT,
         sheetContent = {
